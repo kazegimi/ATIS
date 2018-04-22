cfb3052f63cab3d8f42d2ba4a618c01b8e50c4b0
cdp53190 <cdp53190@gmail.com>
2018-04-22 13:00:34 +0700

    Initial Commit

---
+++ ATIS/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ATIS
+//
+//  Created by Eiichi Hayashi on 2018/04/22.
+//  Copyright © 2018年 skyElements. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ ATIS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ATIS
+//
+//  Created by Eiichi Hayashi on 2018/04/22.
+//  Copyright © 2018年 skyElements. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
